a43c56acad8354d199e284f0fb21ddc9e2fa7f68
-Mayonis masin- <solehinjr@gmail.com>
2017-08-07 20:02:11 +0800

    Update SearchBook.java

--- src/java/SearchBook.java
+++ src/java/SearchBook.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
@@ -21,19 +15,10 @@ import static org.apache.jasper.runtime.JspRuntimeLibrary.include;
 
 /**
  *
- * @author Oops
+ * @author solehin
  */
 public class SearchBook extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
