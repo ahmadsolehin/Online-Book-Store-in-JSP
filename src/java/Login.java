3e73fe8eebfbccf635ee843443833ae856bfa788
-Mayonis masin- <solehinjr@gmail.com>
2017-08-07 20:03:50 +0800

    Update Login.java

--- src/java/Login.java
+++ src/java/Login.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -20,7 +16,7 @@ import javax.servlet.http.HttpSession;
 
 /**
  *
- * @author Oops
+ * @author solehih
  */
 public class Login extends HttpServlet {
 
