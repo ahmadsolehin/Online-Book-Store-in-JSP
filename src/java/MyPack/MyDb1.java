c5041e955a66830e4dc396efebf4b9b0e9e4aca5
-Mayonis masin- <solehinjr@gmail.com>
2017-08-08 23:20:57 +0800

    Update MyDb1.java

--- src/java/MyPack/MyDb1.java
+++ src/java/MyPack/MyDb1.java
@@ -5,15 +5,11 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 /**
  *
- * @author Oops
+ * @author solehin
  */
 public class MyDb1 {
     Connection con;
